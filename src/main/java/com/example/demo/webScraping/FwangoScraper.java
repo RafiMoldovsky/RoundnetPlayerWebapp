2af0f26479017fd4cfa645432998a22785df3cae
Samson Rafi Moldovsky <83265584+RafiMoldovsky@users.noreply.github.com>
2023-08-11 00:00:58 -0400

    Refactoring

--- src/main/java/com/example/demo/webScraping/FwangoScraper.java
+++ src/main/java/com/example/demo/webScraping/FwangoScraper.java
@@ -24,49 +24,54 @@ public class FwangoScraper {
         String url = "https://fwango.io/philadelphia2023";
         driver.manage().window().setSize(new Dimension(1200, 800)); // Set window size
         
-        // try {
-        //     driver.get("url);
-        //     Thread.sleep(2000);
-            
-        //     // Wait for the page to fully load
-        //     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        //     wait.until(webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
-            
-        //     // Find and print initially loaded team names
-        //     List<String> newTeamNames = new ArrayList<>();
-        //     List<String> playerNames = new ArrayList<>();
-        //     getTeamNames(driver, newTeamNames,"", playerNames);
-            
-        //     // Scroll and load more content
-        //     WebElement container = driver.findElement(By.cssSelector("div.TournamentTeamList__Container-sc-13mkiy8-0.irfjog"));
-        //     String lastLoadedTeamName = "";
-        //     long startTime = System.currentTimeMillis();
-        //     long durationMillis = 6000; // 6 seconds - this is a little too much (even for the largest tournaments), but better safe than sorry
-        //     // Make sure to test this duration for richmond
+        processHomePage(driver, url);
+        processResultsPage(driver, url);
 
-        //     while (System.currentTimeMillis() - startTime < durationMillis) {
-        //         int deltaY = 1620; // Adjust the scroll amount as needed
-        //         new Actions(driver)
-        //             .scrollFromOrigin(WheelInput.ScrollOrigin.fromElement(container), 0, deltaY)
-        //             .perform();
+        driver.quit();
+    }
+    public static void processHomePage(WebDriver driver, String url){
+        try {
+            driver.get(url);
+            Thread.sleep(2000);
+            
+            // Wait for the page to fully load
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+            wait.until(webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+            
+            // Find and print initially loaded team names
+            List<String> newTeamNames = new ArrayList<>();
+            List<String> playerNames = new ArrayList<>();
+            getTeamNames(driver, newTeamNames,"", playerNames);
+            
+            // Scroll and load more content
+            WebElement container = driver.findElement(By.cssSelector("div.TournamentTeamList__Container-sc-13mkiy8-0.irfjog"));
+            String lastLoadedTeamName = "";
+            long startTime = System.currentTimeMillis();
+            long durationMillis = 6000; // 6 seconds - this is a little too much (even for the largest tournaments), but better safe than sorry
+            // Make sure to test this duration for richmond
 
-        //         getTeamNames(driver, newTeamNames,lastLoadedTeamName, playerNames);
-        //         lastLoadedTeamName = newTeamNames.get(newTeamNames.size() - 1);   
-        //     }
-        //     List<String> uniqueTeamNames = removeDuplicates(newTeamNames);
-        //     List<String> uniquePlayerNames = removeDuplicates(playerNames);
+            while (System.currentTimeMillis() - startTime < durationMillis) {
+                int deltaY = 1620; // Adjust the scroll amount as needed
+                new Actions(driver)
+                    .scrollFromOrigin(WheelInput.ScrollOrigin.fromElement(container), 0, deltaY)
+                    .perform();
+
+                getTeamNames(driver, newTeamNames,lastLoadedTeamName, playerNames);
+                lastLoadedTeamName = newTeamNames.get(newTeamNames.size() - 1);   
+            }
+            List<String> uniqueTeamNames = removeDuplicates(newTeamNames);
+            List<String> uniquePlayerNames = removeDuplicates(playerNames);
         
-        //     for (int i=0; i<uniqueTeamNames.size(); i++) {
-        //         System.out.println(uniqueTeamNames.get(i));
-        //         System.out.println(uniquePlayerNames.get(i));
-        //     }
+            for (int i=0; i<uniqueTeamNames.size(); i++) {
+                System.out.println(uniqueTeamNames.get(i));
+                System.out.println(uniquePlayerNames.get(i));
+            }
 
-        // } catch (Exception e) {
-        //     e.printStackTrace();
-        // } finally {
-        //     driver.quit();
-        // }
-
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+    }
+    public static void processResultsPage(WebDriver driver, String url){
         // Now go to results page
 
            try {
@@ -223,9 +228,7 @@ public class FwangoScraper {
 
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            driver.quit();
-        }
+        } 
     }
     
     private static List<String> getTeamNames(WebDriver driver, List<String> teamNames, String lastTeamName, List<String> playerNames) {
