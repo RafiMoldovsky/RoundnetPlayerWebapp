7fac111f2af1e467a3c82cf78fab6b4bb7d2f775
Cory Chilton <coryhookchilton@gmail.com>
2023-08-13 12:13:42 -0700

    Implemented player searching

---
+++ roundnet-player-web-app/data/players.tsx
@@ -0,0 +1,82 @@
+export const players: playerInterface[] = [
+  {
+    name: 'Frederic Hinkle',
+  },
+  {
+    name: 'Will Picone',
+  },
+  {
+    name: 'Rahul Murthy',
+  },
+  {
+    name: 'Grant Laughlin',
+  },
+  {
+    name: 'Dan Abrams',
+  },
+  {
+    name: 'Grant Klapwijk',
+  },
+  {
+    name: 'Matthew Cole',
+  },
+  {
+    name: 'Vincent Mathieu',
+  },
+  {
+    name: 'Max Model',
+  },
+  {
+    name: 'Cole Model',
+  },
+  {
+    name: 'Gabriel Finocchi',
+  },
+  {
+    name: 'Thomas Hamilton',
+  },
+  {
+    name: 'Ryan Marino',
+  },
+  {
+    name: 'Clark Marshall',
+  },
+  {
+    name: 'Ali Jenki',
+  },
+  {
+    name: 'Christian Bennett',
+  },
+  {
+    name: 'Noah Luskus',
+  },
+  {
+    name: 'Ryan Gross',
+  },
+  {
+    name: 'Joe Bondi',
+  },
+  {
+    name: 'Rafi Moldovsky',
+  },
+  {
+    name: 'Sarah Allen',
+  },
+  {
+    name: 'Kalin Morgan',
+  },
+  {
+    name: 'Katie Pierson',
+  },
+  {
+    name: 'Olivia Jenki',
+  },
+  {
+    name: 'Allie Foster',
+  },
+  
+]
+
+export interface playerInterface {
+  name: string;
+}
--- roundnet-player-web-app/src/app/players/page.tsx
+++ roundnet-player-web-app/src/app/players/page.tsx
@@ -1,10 +1,33 @@
+'use client'
+import { useState } from "react"
+import { players, playerInterface } from "../../../data/players";
+import Link from "next/link";
+
 export default function Players(){
+  // state to control player search value
+  const [searchInput, setSearchInput] = useState('');
+
+  //Making an array of the players that should show up on this search
+  const searchedPlayers: playerInterface[] = [];
+
+  for (var i in players) {
+    if(players[i].name.toLowerCase().includes(searchInput.toLowerCase())){
+      console.log(players[i].name);
+      searchedPlayers.push(players[i]);
+    }
+  }
+  
   return(
     <>
       <div>Players</div>
-      <ul>
-        <li>Frederic Hinkle</li>
-      </ul>
+      <input className="border border-black" placeholder="Enter Player's Name" value={searchInput} onChange={(e) => setSearchInput(e.target.value)}/>
+      {searchedPlayers.map(player => 
+        <Link key={player.name} href={`/players/${player.name}`}>
+          <div>
+            {player.name}
+          </div>
+        </Link>
+      )}
     </>
   )
 }
