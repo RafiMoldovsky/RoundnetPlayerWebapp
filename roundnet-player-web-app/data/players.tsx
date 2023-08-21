aa5f80ebf75c0c80d306d0bfe147fd1d44639c21
Cory Chilton <coryhookchilton@gmail.com>
2023-08-21 12:43:17 -0700

    changed player data structure to map

--- roundnet-player-web-app/data/players.tsx
+++ roundnet-player-web-app/data/players.tsx
@@ -1,109 +1,27 @@
-export const players: playerInterface[] = [
-  {
-    name: 'Frederic Hinkle',
+export const players = new Map([
+  ["1", {
+      id: 1,
+      name: 'Rafi Moldovsky',
+  }],
+  ["1", {
     id: 1,
-
-  },
-  {
-    name: 'Will Picone',
-    id: 2,
-  },
-  {
-    name: 'Rahul Murthy',
-    id: 3,
-  },
-  {
-    name: 'Grant Laughlin',
-    id: 4,
-  },
-  {
-    name: 'Dan Abrams',
-    id: 5,
-  },
-  {
-    name: 'Grant Klapwijk',
-    id: 6,
-  },
-  {
-    name: 'Matthew Cole',
-    id: 7,
-  },
-  {
-    name: 'Vincent Mathieu',
-    id: 8,
-  },
-  {
-    name: 'Max Model',
-    id: 9,
-  },
-  {
-    name: 'Cole Model',
-    id: 10,
-  },
-  {
-    name: 'Gabriel Finocchi',
-    id: 11,
-  },
-  {
-    name: 'Thomas Hamilton',
-    id: 12,
-  },
-  {
-    name: 'Ryan Marino',
-    id: 13,
-  },
-  {
-    name: 'Clark Marshall',
-    id: 14,
-  },
-  {
-    name: 'Ali Jenki',
-    id: 15,
-  },
-  {
-    name: 'Christian Bennett',
-    id: 16,
-  },
-  {
-    name: 'Noah Luskus',
-    id: 17,
-  },
-  {
-    name: 'Ryan Gross',
-    id: 18,
-  },
-  {
-    name: 'Joe Bondi',
-    id: 19,
-  },
-  {
     name: 'Rafi Moldovsky',
-    id: 20,
-  },
-  {
-    name: 'Sarah Allen',
-    id: 21,
-  },
-  {
-    name: 'Kalin Morgan',
-    id: 22,
-  },
-  {
-    name: 'Katie Pierson',
-    id: 23,
-  },
-  {
-    name: 'Olivia Jenki',
-    id: 24,
-  },
-  {
-    name: 'Allie Foster',
-    id: 25,
-  },
-  
-]
+  }],
+  ["2", {
+    id: 2,
+    name: 'Kyan Kornfeld',
+  }],
+  ["3", {
+    id: 3,
+    name: 'William Foote',
+  }], 
+  ["4", {
+    id: 4,
+    name: 'Arrow Griner',
+  }], 
+]);
 
 export interface playerInterface {
-  name: string;
   id: number;
+  name: string;
 }
