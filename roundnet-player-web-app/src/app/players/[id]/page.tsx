815f4ae33210257df17e921d20ba88305f392125
Cory Chilton <coryhookchilton@gmail.com>
2023-08-21 14:23:53 -0700

    bare bones stat meters for each player

--- roundnet-player-web-app/src/app/players/[id]/page.tsx
+++ roundnet-player-web-app/src/app/players/[id]/page.tsx
@@ -11,9 +11,21 @@ export default function PlayerPage({params}: pageProps){
   if(!isAPlayer(player)) {
     return <div>PLAYER NOT FOUND!</div>;
   }
+  const PERCENT_GAME_WINS = Math.round(player.games_won / (player.games_won + player.games_lost) * 100);
+  const PERCENT_SERIES_WINS = Math.round(player.series_won / (player.series_won + player.series_lost) * 100);
+  const PERCENT_POINT_WINS = Math.round(player.points_won / (player.points_won + player.points_lost) * 100);
   return (
-    <>
-      <div>{player.player_name}</div>
-    </>
+    <div>
+      <h1 className="text-center text-xl font-bold mb-10">{player.player_name}'s Stats</h1>
+      <div className="flex justify-around">
+        <StatMeter percent={PERCENT_GAME_WINS} label="Game Wins" />
+        <StatMeter percent={PERCENT_SERIES_WINS} label="Series Wins" />
+        <StatMeter percent={PERCENT_POINT_WINS} label="Point Wins" />
+      </div>
+    </div>
   )
+}
+
+function StatMeter({percent, label}:{percent: number, label: string}){
+  return <div>{label}: {percent}%</div>
 }
--- roundnet-player-web-app/src/components/Navbar.tsx
+++ roundnet-player-web-app/src/components/Navbar.tsx
@@ -8,14 +8,14 @@ export default function Navbar() {
           Home
         </button>
       </Link>
-      <Link href={"/connections"}>
-        <button>
-          Connections
-        </button>
-      </Link>
       <Link href={"/players"}>
         <button>
           Players
+        </button>
+      </Link>
+      <Link href={"/connections"}>
+        <button>
+          Connections
         </button>
       </Link>
     </div>
