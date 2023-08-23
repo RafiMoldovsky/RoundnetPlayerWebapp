2cb9a0b2ca2ff9686af8f4c6a90c333ae74e4450
Cory Chilton <coryhookchilton@gmail.com>
2023-08-22 22:12:46 -0700

    Added CORS to graph and player controller

--- backend/src/main/java/backend/backend/controller/PlayerController.java
+++ backend/src/main/java/backend/backend/controller/PlayerController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import backend.backend.model.Player;
 import backend.backend.repositories.PlayerRepository;
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:3000", "https://roundnetplayer.vercel.app/"})
 @RestController
 @RequestMapping("/api")
 public class PlayerController {
